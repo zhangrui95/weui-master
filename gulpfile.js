6990b2351cf35f7d528cb64547e7051b5649adcc
wangxiaodong <wangxiaodong@ys.com>
2016-12-20 10:18:48 +0800

    gsp build

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@ var tap = require('gulp-tap');
 var nano = require('gulp-cssnano');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var processhtml = require('gulp-processhtml');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var pkg = require('./package.json');
@@ -25,11 +27,16 @@ var yargs = require('yargs')
         'p': {
             alias: 'port',
             type: 'number'
+        },
+        'g': {
+            alias: 'gsp',
+            type: 'boolean'
         }
     }).argv;
 
 var option = {base: 'src'};
 var dist = __dirname + '/dist';
+var gspDist = dist + '/gsp';
 
 gulp.task('build:style', function (){
     var banner = [
@@ -60,10 +67,58 @@ gulp.task('build:style', function (){
         .pipe(gulp.dest(dist));
 });
 
+gulp.task('gsp:build:style', function (){
+    var banner = [
+        '/*!',
+        ' * WeUI v<%= pkg.version %> (<%= pkg.homepage %>)',
+        ' * Copyright <%= new Date().getFullYear() %> Tencent, Inc.',
+        ' * Licensed under the <%= pkg.license %> license',
+        ' */',
+        ''].join('\n');
+    gulp.src('src/style/weui.less', option)
+        .pipe(sourcemaps.init())
+        .pipe(less().on('error', function (e) {
+            console.error(e.message);
+            this.emit('end');
+        }))
+        .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(gspDist))
+        .pipe(nano({
+            zindex: false,
+            autoprefixer: false
+        }))
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest(gspDist));
+});
+
+gulp.task('build:gsp:assets', function (){
+    gulp.src('src/example/**/*.?(png|jpg|gif|js)', option)
+        .pipe(gulp.dest(gspDist));
+});
+
 gulp.task('build:example:assets', function (){
     gulp.src('src/example/**/*.?(png|jpg|gif|js)', option)
         .pipe(gulp.dest(dist))
         .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('build:gsp:style', function (){
+    gulp.src('src/example/example.less', option)
+        .pipe(less().on('error', function (e){
+            console.error(e.message);
+            this.emit('end');
+        }))
+        .pipe(replace('images/',''))
+        .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
+        .pipe(nano({
+            zindex: false,
+            autoprefixer: false
+        }))
+        .pipe(gulp.dest(gspDist));
 });
 
 gulp.task('build:example:style', function (){
@@ -82,32 +137,57 @@ gulp.task('build:example:style', function (){
 });
 
 gulp.task('build:example:html', function (){
-    var entryBuild = function (target) {
-        gulp.src(target, option)
-            .pipe(tap(function (file){
-                var dir = path.dirname(file.path);
-                var contents = file.contents.toString();
-                contents = contents.replace(/<link\s+rel="import"\s+href="(.*)">/gi, function (match, $1){
-                    var filename = path.join(dir, $1);
-                    var id = path.basename(filename, '.html');
-                    var content = fs.readFileSync(filename, 'utf-8');
-                    return '<script type="text/html" id="tpl_'+ id +'">\n'+ content +'\n</script>';
-                });
-                file.contents = new Buffer(contents);
-            }))
-            .pipe(gulp.dest(dist))
-            .pipe(browserSync.reload({stream: true}));
-    };
-    entryBuild('src/example/index.html');
-    entryBuild('src/example/dev.html');
-    entryBuild('src/example/hotel.html');
-    entryBuild('src/example/leader.html');
-    entryBuild('src/example/police.html');
+    gulp.src('src/example/*.html', option)
+        .pipe(tap(function (file){
+            var dir = path.dirname(file.path);
+            var contents = file.contents.toString();
+            contents = contents.replace(/<link\s+rel="import"\s+href="(.*)">/gi, function (match, $1){
+                var filename = path.join(dir, $1);
+                var id = path.basename(filename, '.html');
+                var content = fs.readFileSync(filename, 'utf-8');
+                return '<script type="text/html" id="tpl_'+ id +'">\n'+ content +'\n</script>';
+            });
+            file.contents = new Buffer(contents);
+        }))
+        .pipe(gulp.dest(dist))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('build:example:gsp', function (){
+    gulp.src('src/example/*.html', option)
+        .pipe(processhtml())
+        .pipe(replace('../images','assets'))
+        .pipe(tap(function (file){
+            var dir = path.dirname(file.path);
+            var subDir = path.basename(file.path, '.html');
+            var contents = file.contents.toString();
+            contents = contents.replace(/<!\-\-<build:exclude>\-\->(.*)<!\-\-<\/build:exclude>\-\->/gi, function (match, $1){
+                console.log($1)
+                return $1;
+            });
+            contents = contents.replace(/<link\s+rel="import"\s+href="(.*)">/gi, function (match, $1){
+                var filename = path.join(dir, $1);
+                var id = path.basename(filename, '.html');
+                gulp.src(filename)
+                    .pipe(replace('../images','assets'))
+                    .pipe(rename({basename:'_'+id,extname:'.gsp'}))
+                    .pipe(gulp.dest(gspDist+'/example/'+subDir));
+                var content = '<g:render template="'+subDir+'/'+id+'"/>';
+                return '<script type="text/html" id="tpl_'+ id +'">'+ content +'</script>';
+            });
+            file.contents = new Buffer(contents);
+        }))
+        .pipe(rename({extname:'.gsp'}))
+        .pipe(gulp.dest(gspDist));
 });
 
 gulp.task('build:example', ['build:example:assets', 'build:example:style', 'build:example:html']);
 
 gulp.task('release', ['build:style', 'build:example']);
+
+gulp.task('build:gsp', ['build:gsp:assets', 'build:gsp:style', 'build:example:gsp']);
+
+gulp.task('gsp', ['gsp:build:style', 'build:gsp']);
 
 gulp.task('watch', ['release'], function () {
     gulp.watch('src/style/**/*', ['build:style']);
@@ -144,5 +224,9 @@ gulp.task('default', ['release'], function () {
 
     if (yargs.w) {
         gulp.start('watch');
+    }
+
+    if (yargs.g) {
+        gulp.start('gsp');
     }
 });
