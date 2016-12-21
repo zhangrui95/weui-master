90f99733423e0017ad0a83ee7c2c693429dcfd37
wangxiaodong <wangxiaodong@ys.com>
2016-12-21 11:59:45 +0800

    add api mock

--- test/mock.js
+++ test/mock.js
@@ -38,4 +38,11 @@ module.exports = [
             ]));
         }
     },
+    {
+        route: "/example/api/user/jsTicket.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify({appid:'test_appid',timestamp:new Date().getTime(),nonceStr:'test_nonceStr',signature:'test_signature'}));
+        }
+    }
 ];
