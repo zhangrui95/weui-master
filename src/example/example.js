88529b0d69c13abcd3624aa4297e22e3415f8acf
zhangrui <zhangrui@ys.com>
2017-01-06 16:27:30 +0800

    将数字动画效果=>example.js

--- src/example/example.js
+++ src/example/example.js
@@ -330,19 +330,91 @@ function getLocalTime(nS) {
     min = min<10?('0'+min):min;
     second = second<10?('0'+second):second;
     return date.getFullYear()+'-'+month+'-'+day + ' '+hours+':'+min+':'+second
+};
+//从0到总数动画效果
+function animates(){
+    $.fn.countTo = function (options) {
+        options = options || {};
+
+        return $(this).each(function () {
+            // set options for current element
+            var settings = $.extend({}, $.fn.countTo.defaults, {
+                from:            $(this).data('from'),
+                to:              $(this).data('to'),
+                speed:           $(this).data('speed'),
+                refreshInterval: $(this).data('refresh-interval'),
+                decimals:        $(this).data('decimals')
+            }, options);
+            // how many times to update the value, and how much to increment the value on each update
+            var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                increment = (settings.to - settings.from) / loops;
+
+            // references & variables that will change with each update
+            var self = this,
+                $self = $(this),
+                loopCount = 0,
+                value = settings.from,
+                data = $self.data('countTo') || {};
+            $self.data('countTo', data);
+
+            // if an existing interval can be found, clear it first
+            if (data.interval) {
+                clearInterval(data.interval);
+            }
+            data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+            // initialize the element with the starting value
+            render(value);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+
+                render(value);
+
+                if (typeof(settings.onUpdate) == 'function') {
+                    settings.onUpdate.call(self, value);
+                }
+
+                if (loopCount >= loops) {
+                    // remove the interval
+                    $self.removeAttr('countTo');
+                    clearInterval(data.interval);
+                    value = settings.to;
+
+                    if (typeof(settings.onComplete) == 'function') {
+                        settings.onComplete.call(self, value);
+                    }
+                }
+            }
+
+            function render(value) {
+                var formattedValue = settings.formatter.call(self, value, settings);
+                $self.html(formattedValue);
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 0,               // the number the element should start at
+        to: 0,                 // the number the element should end at
+        speed: 1000,           // how long it should take to count between the target numbers
+        refreshInterval: 100,  // how often the element should be updated
+        decimals: 0,           // the number of decimal places to show
+        formatter: formatter,  // handler for formatting the value before rendering
+        onUpdate: null,        // callback method for every time the element is updated
+        onComplete: null       // callback method for when the element finishes updating
+    };
+
+    function formatter(value, settings) {
+        return value.toFixed(settings.decimals);
+    }
+    // start all the timers
+    $('.timer').each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
 }
-// function getLocalTimes(nS) {
-//     if(nS==null){
-//         return '';
-//     }
-//     var date = new Date(nS);
-//     var hours = date.getHours();
-//     var min = date.getMinutes();
-//     var second = date.getSeconds();
-//     var tag = hours>12?'pm':'am';
-//     hours = hours>12?(hours-12):hours;
-//     hours = hours<10?('0'+hours):hours;
-//     min = min<10?('0'+min):min;
-//     second = second<10?('0'+second):second;
-//     return hours+':'+min+':'+second+' '+tag
-// }
