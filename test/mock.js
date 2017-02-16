de8c831fe5dbabcd81efff0694df6328cf10a98b
zhangrui <zhangrui@ys.com>
2017-02-16 17:31:03 +0800

    change

--- test/mock.js
+++ test/mock.js
@@ -415,8 +415,8 @@ module.exports = [
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify(
                 {
-                    company:{name:'瓦力科技',address:'南岗区嵩山路19号',type:{name:"软件"},police:{name:'林琳'},groupType:0,fireState:0},
-                    users:[{name:'苏三',mobile:'13936003636',id:'link1'},{name:'西维维尔',mobile:'13936003636',id:'link2'}]
+                    company:{name:'瓦力科技',address:'南岗区嵩山路19号',type:{name:"软件",id:'k1'},police:{name:'林琳',id:'p1'},groupType:0,fireState:0},
+                    users:[{name:'苏三',mobile:'13936003636',id:'link1',wid:'122333'},{name:'西维维尔',mobile:'13936003636',id:'link2',email:'123@qq.ocm',post:'其他'}]
                 }
 
             ));
