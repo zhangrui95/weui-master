30a5150faee03cae58beb399f324dc25f4f90710
wangxiaodong <wangxiaodong@ys.com>
2017-02-17 15:56:49 +0800

    fixed user company data change

--- test/mock.js
+++ test/mock.js
@@ -7,18 +7,18 @@ module.exports = [
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify(
                 {list:[
-                    {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                    {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                    {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                    {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832},
-                    {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                    {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                    {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                    {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832},
-                    {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                    {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                    {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                    {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832}
+                    {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                    {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                    {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                    {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832},
+                    {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                    {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                    {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                    {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832},
+                    {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                    {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                    {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                    {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832}
                 ],
                     undo:19,
                     done:37
@@ -31,18 +31,18 @@ module.exports = [
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify({list:[
-                {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832},
-                {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832},
-                {id:'key1', card:'230105199603022201', hotelName:'禧龙宾馆1', photo:'images/header.jpg', state:'主键', user:{uid:'100',company:'禧龙宾馆1', name:'张三', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2301', name:'民警1'}, doneTime:1482214046832},
-                {id:'key2', card:'230105199603022202', hotelName:'禧龙宾馆2', photo:'images/header.jpg', state:'主键2', user:{uid:'200',company:'旅店2', name:'张三2', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2302', name:'民警2'}, doneTime:1482214046832},
-                {id:'key3', card:'230105199603022203', hotelName:'禧龙宾馆3', photo:'images/header.jpg', state:'主键', user:{uid:'300',company:'旅店3', name:'张三3', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2303', name:'民警3'}, doneTime:1482214046832},
-                {id:'key4', card:'230105199603022204', hotelName:'禧龙宾馆4', photo:'images/header.jpg', state:'主键2', user:{uid:'400',company:'旅店4', name:'张三4', mobile:'13012345678'}, createTime:1482214046832, doneUser:{uid:'2304', name:'民警4'}, doneTime:1482214046832}
+                {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832},
+                {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832},
+                {id:'key1', card:'230105199603022201', company:'禧龙宾馆1', createTime:1482214046832},
+                {id:'key2', card:'230105199603022202', company:'旅店2', createTime:1482214046832},
+                {id:'key3', card:'230105199603022203', company:'旅店3', createTime:1482214046832},
+                {id:'key4', card:'230105199603022204', company:'旅店4', createTime:1482214046832}
             ],
                 undo:10,
                 done:55
@@ -64,7 +64,7 @@ module.exports = [
         route: "/example/api/task/leaderDetail.json",
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
-            var ret = {id:'key1', card:'230105199603022201', user:{uid:'100',company:'禧龙宾馆1',address:'xx路yy号', name:'张三', mobile:'13012345678'}, photo:'images/header.jpg', state:'0', createTime:1482214046832, doneUser:null, doneTime:null};
+            var ret = {id:'key1', card:'230105199603022201', user:{uid:'100', name:'张三', mobile:'13012345678'},company:{name:'禧龙宾馆1',address:'xx路yy号'}, photo:'images/header.jpg', state:'0', createTime:1482214046832, doneUser:null, doneTime:null};
             ret.state = '1';
             ret.doneUser = {uid:'2301', name:'民警1'};
             ret.doneTime = 1482214046832;
