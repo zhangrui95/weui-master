b418ce318893730f912f18293db788904d56d2de
zhangrui <zhangrui@ys.com>
2017-03-16 13:46:01 +0800

    add

--- test/mock.js
+++ test/mock.js
@@ -738,7 +738,7 @@ module.exports = [
         route: "/example/api/floor/save.json",
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
-            res.end(JSON.stringify({state:'1'}));
+            res.end(JSON.stringify({state:'1',floorid:'id1'}));
         }
     },
     {
@@ -747,12 +747,12 @@ module.exports = [
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify(
                [
-                   {id:'r1',street:'红旗大街',num:389,ridgepole:3,unit:2,door:402,tenants:[]},
-                   {id:'r2',street:'嵩山路',num:21-19,ridgepole:4,unit:5,door:712,tenants:[]},
-                   {id:'r3',street:'香江路',num:3,ridgepole:1,unit:1,door:323,tenants:[]},
-                   {id:'r4',street:'珠江路',num:119,ridgepole:6,unit:2,door:212,tenants:[]},
-                   {id:'r5',street:'中山路',num:84,ridgepole:5,unit:4,door:506,tenants:[]},
-                   {id:'r6',street:'华山路',num:59,ridgepole:'',unit:6,door:721,tenants:[]},
+                   {id:'r1',street:'红旗大街',num:'389',ridgepole:3,unit:2,door:402,tenants:[]},
+                   {id:'r2',street:'嵩山路',num:'21-19',ridgepole:4,unit:5,door:712,tenants:[]},
+                   {id:'r3',street:'香江路',num:'3',ridgepole:1,unit:1,door:323,tenants:[]},
+                   {id:'r4',street:'珠江路',num:'119',ridgepole:6,unit:2,door:212,tenants:[]},
+                   {id:'r5',street:'中山路',num:'84',ridgepole:5,unit:4,door:506,tenants:[]},
+                   {id:'r6',street:'华山路',num:'59',ridgepole:'',unit:6,door:721,tenants:[]},
                ]
             ));
         }
@@ -769,9 +769,7 @@ module.exports = [
                     {sfzh: "230105199632020201",date:1489551285959,id:'re4'},
                     {sfzh: "230105199632020201",date:1489551285959,id:'re5'},
                     {sfzh: "230105199632020201",date:1489551285959,id:'re6'},
-                    {sfzh: "230105199632020201",date:1489551285959,id:'re7'},
-                    {sfzh: "230105199632020201",date:1489551285959,id:'re8'},
-                    {sfzh: "230105199632020201",date:1489551285959,id:'re9'},
+                    {sfzh: "230105199632020201",date:1489551285959,id:'re7'}
                 ]
             ));
         }
@@ -781,7 +779,7 @@ module.exports = [
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify(
-                {count:'9'}
+                {count:'7'}
             ));
         }
     },
@@ -790,9 +788,27 @@ module.exports = [
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify(
-                {count:'12'}
+                {count:'6'}
             ));
         }
-    }
+    },
+    {
+        route: "/example/api/tenant/find.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify(
+                    {id:'re1',date:1489551285959,floor:{id:'f1',community:{name:'红旗社区'},street:'红旗大街',num:18,ridgepole:4,unit:1,door:402},phone:13936003725,photo:'images/header.jpg',sfzh:'230105199804262325'}
+            ));
+        }
+    },
+    {
+        route: "/example/api/tenant/cancel.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify(
+                {state:'1'}
+            ));
+        }
+    },
 
 ];
