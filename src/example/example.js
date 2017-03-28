9d78c4826d95e9d58ad3a7d424252f1348919b54
wangxiaodong <wangxiaodong@ys.com>
2017-03-28 16:56:07 +0800

    merge js & css

--- src/example/example.js
+++ src/example/example.js
@@ -706,3 +706,17 @@ var nullToEmpty = function (obj) {
     }
     return obj;
 }
+
+/**
+ * 生成UUID
+ * @returns {string}
+ */
+var generateUUID = function () {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+};
