2e2d054b4e08a9bce27f69da2e232cfcfd8a346f
wangxiaodong <wangxiaodong@ys.com>
2017-03-31 10:24:35 +0800

    replace sep

--- test/mocks/menu.js
+++ test/mocks/menu.js
@@ -22,10 +22,10 @@ var getPoliceMenu= function (buf) {
         {name:"出租房管理",hash:"rent",icon:"images/icon7.png"},
         {name:"所内工作",hash:"police-dynamic",icon:"images/icon11.png"}
     ],data;
-    if(buf.indexOf('target=dept_police%7CCAdmin') != -1){
+    if(buf.indexOf('target=dept_police-CAdmin') != -1){
         menu = [{name:"企业管理",href:"leader.html",hash:"clear_data",icon:"images/icon9.png"}].concat(menu)
         data = {policeId:'xx',name:'苏素素',avatar:'images/head.jpg?'};
-    }else if(buf.indexOf('target=dept_police%7CRAdmin') != -1){
+    }else if(buf.indexOf('target=dept_police-RAdmin') != -1){
         menu = [
             {name:"企业统计",hash:"police-clear-company",icon:"images/icon9.png"},
             {name:"设置值班民警",href:"leader.html",hash:"Set_up",icon:"images/icon1.png","data":{rota:[{id:'r1',user:{id:'key3',name:"傅志强"}}]}}
@@ -41,9 +41,9 @@ var getCompanyMenu= function (buf) {
     var menu = [
         {name:"从业人员上报",hash:"employee-report",icon:"images/icon3.png"}
     ];
-    if(buf.indexOf('target=dept_company%7CHotel') != -1){
+    if(buf.indexOf('target=dept_company-Hotel') != -1){
         menu = [{name:"未带身份证入住",href:"hotel.html",icon:"images/icon2.png"}].concat(menu)
-    }else if(buf.indexOf('target=dept_company%7COil') != -1){
+    }else if(buf.indexOf('target=dept_company-Oil') != -1){
         menu = menu.concat([
             {name:"散装油交易登记",href:"station.html",icon:"images/icon12.png"}
         ])
