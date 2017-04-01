c1a66a60e77e201f22e9f2dacb89ee979f44fe59
wangxiaodong <wangxiaodong@ys.com>
2017-04-01 13:56:23 +0800

    console company type api change

--- test/mocks/companyType.js
+++ test/mocks/companyType.js
@@ -6,6 +6,21 @@ var routes = [
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify( [
+                {id:'t1',name:'饭店'},
+                {id:'t2',name:'幼儿园'},
+                {id:'t3',name:'医疗'},
+                {id:'t4',name:'洗浴'},
+                {id:'t5',name:'旅店'},
+                {id:'t6',name:'汽配'},
+                {id:'t7',name:'律师事务所'}
+            ]));
+        }
+    },
+    {
+        route: "/example/api/companyType/list.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify( [
                 {id:'t1',name:'饭店',wxDeptId:1},
                 {id:'t2',name:'幼儿园',wxDeptId:2},
                 {id:'t3',name:'医疗',wxDeptId:3},
