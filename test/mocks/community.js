bc70018f6e6d8abc394949afb283a0c5b626384d
wangxiaodong <wangxiaodong@ys.com>
2017-04-06 14:01:34 +0800

    add community to console

---
+++ test/mocks/community.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var routes = [
+    {
+        route: "/example/api/community.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify( [
+                {id:'sq1',name:'金和社区'},
+                {id:'sq2',name:'信定社区'},
+                {id:'sq3',name:'红色社区'}
+            ]));
+        }
+    },
+    {
+        route: "/example/api/community/save.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify({id:'sq4',name:'新增社区'}));
+        }
+    },
+    {
+        route: "/example/api/community/update.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify({id:'sq5',name:'修改社区'}));
+        }
+    }
+];
+
+module.exports = routes;
