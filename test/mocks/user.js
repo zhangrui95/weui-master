3923de3ff114f36d53f0829e1369d314988c43b5
wangxiaodong <wangxiaodong@ys.com>
2017-04-10 16:02:55 +0800

    console user api change role to list

--- test/mocks/user.js
+++ test/mocks/user.js
@@ -77,6 +77,27 @@ var routes = [
         }
     },
     {
+        route: "/example/api/user/list.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify([
+                {name:'艾一一',avatar:'images/header.jpg?',id:'key1',phonetic:'AiYiYi'},
+                {name:'陈二二',avatar:'',id:'key2',phonetic:'C'},
+                {name:'傅志强',avatar:'images/header.jpg?',id:'key3',phonetic:'F'},
+                {name:'关四四',avatar:'images/header.jpg?',id:'key4',phonetic:'G'},
+                {name:'李一一',avatar:'images/header.jpg?',id:'key5',phonetic:'L'},
+                {name:'李二二',avatar:'images/header.jpg?',id:'key6',phonetic:'L'},
+                {name:'谭三三',avatar:'images/header.jpg?',id:'key7',phonetic:'T'},
+                {name:'王四四',avatar:'images/header.jpg?',id:'key8',phonetic:'W'},
+                {name:'王一一',avatar:'images/header.jpg?',id:'key9',phonetic:'W'},
+                {name:'赵二二',avatar:'images/header.jpg?',id:'key10',phonetic:'Z'},
+                {name:'张三三',avatar:'images/header.jpg?',id:'key11',phonetic:'Z'},
+                {name:'张四四',avatar:'images/header.jpg?',id:'key12',phonetic:'ZhangSiSi'},
+                {name:'钱鑫鑫',avatar:'images/header.jpg?',id:'key12',phonetic:'QianXinXin'}
+            ]));
+        }
+    },
+    {
         route: "/example/api/user/save.json",
         handle: function (req, res, next) {
             res.setHeader('Content-Type','application/json; charset=UTF-8');
