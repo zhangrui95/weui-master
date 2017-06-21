5176ed705a414d504abd4162e8655ac56e816a14
wangxiaodong <wangxiaodong@ys.com>
2017-06-21 10:37:04 +0800

    add inner api mock

--- test/mocks/appConfig.js
+++ test/mocks/appConfig.js
@@ -13,6 +13,20 @@ var routes = [
             res.setHeader('Content-Type','application/json; charset=UTF-8');
             res.end(JSON.stringify({state:0}));
         }
+    },
+    {
+        route: "/example/api/appConfig/addInnerUser.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify({state:1}));
+        }
+    },
+    {
+        route: "/example/api/appConfig/setInnerTimeWindow.json",
+        handle: function (req, res, next) {
+            res.setHeader('Content-Type','application/json; charset=UTF-8');
+            res.end(JSON.stringify({state:1}));
+        }
     }
 ];
 
